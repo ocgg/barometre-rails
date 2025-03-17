0fc47930e76654f26fdc9cac96755d9bbdd0c9c7
ocgg <olivier.g@lavache.com>
2025-03-17 11:19:27 +0100

    add role to user in generated migration file from rails auth

--- db/migrate/20250317095209_create_users.rb
+++ db/migrate/20250317095209_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
+      t.string :role, null: false, default: "user"
 
       t.timestamps
     end
