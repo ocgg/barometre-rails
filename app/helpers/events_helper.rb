03d515d1faf73da5825026e698521e7e766f315d
ocgg <olivier.g@lavache.com>
2025-03-18 20:06:56 +0100

    locales fr & helper format date

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,8 @@
 module EventsHelper
+  def format_date(date)
+    return "Aujourd'hui" if date == Date.today
+    return "Demain" if date == Date.tomorrow
+
+    l(date, format: "%A %d %B").split.map(&:capitalize).join(" ")
+  end
 end
