97d10328060aeb9eddc4acbf995ca0f125263897
Olivier G <zotri@hotmail.com>
2025-04-25 18:45:49 +0200

    stable

--- app/javascript/controllers/datefilter_controller.js
+++ app/javascript/controllers/datefilter_controller.js
@@ -12,21 +12,28 @@ export default class extends Controller {
   ]
 
   connect() {
-    this.today = new Date();
-    this.year = this.today.getFullYear();
-    this.month = this.today.getMonth();
+    this.inputTarget.value = '';
+    this.currentDate = new Date();
+    this.currentDate.setHours(0, 0, 0, 0);
 
     this.startDate = null;
     this.endDate = null;
 
-    const dayEltClasses = "flex h-[46px] w-[46px] items-center justify-center rounded-full hover:bg-baro-yellow mb-2 cursor-pointer"
+    const colors = {
+      primary: "fgcolor",
+      secondary: "bgcolor",
+      faded: "fgcolor-faded",
+      highlight: "baro-yellow",
+    }
+    const dayEltClasses = `flex h-[46px] w-[46px] items-center justify-center rounded-full hover:bg-${colors.highlight} mb-1 cursor-pointer`
 
     this.cssClasses = {
       currentMonthDay: dayEltClasses,
-      prevMonthDay: dayEltClasses + " text-fgcolor-faded",
-      selectedDay: dayEltClasses + " bg-fgcolor-faded text-bgcolor rounded-none",
-      selectedStartDay: dayEltClasses + " bg-fgcolor text-bgcolor rounded-r-none",
-      selectedEndDay: dayEltClasses + " bg-fgcolor text-bgcolor rounded-l-none",
+      otherMonthDay: `${dayEltClasses} text-${colors.faded}`,
+      selectedDay: `${dayEltClasses} bg-${colors.primary} text-${colors.secondary} rounded-none`,
+      selectedStartDay: `${dayEltClasses} bg-${colors.primary} text-${colors.secondary} rounded-r-none`,
+      selectedEndDay: `${dayEltClasses} bg-${colors.primary} text-${colors.secondary} rounded-l-none`,
+      onlySelectedDay: `${dayEltClasses} bg-${colors.primary} text-${colors.secondary} rounded-full`
     }
 
     // Close datepicker when clicking outside
@@ -40,62 +47,53 @@ export default class extends Controller {
   renderCalendar() {
     this.daysContainerTarget.innerHTML = '';
     this.displayMonthName();
-    this.renderPrevMonthDays();
-    this.renderCurrentMonthDays();
+    this.renderDays();
   }
 
   displayMonthName() {
     const opts = { month: 'long', year: 'numeric' };
-    let monthAndYear = this.today.toLocaleDateString('fr-FR', opts);
+    let monthAndYear = this.currentDate.toLocaleDateString('fr-FR', opts);
     this.currentMonthTarget.textContent = this.capitalize(monthAndYear);
   }
 
   capitalize(string) { return string.charAt(0).toUpperCase() + string.slice(1); }
 
-  renderPrevMonthDays() {
-    const firstWeekDayOfMonth = new Date(this.year, this.month, 0).getDay();
+  renderDays() {
+    const firstMonthDate = new Date(this.currentDate)
+    firstMonthDate.setDate(0)
+    const firstWeekDayOfMonth = firstMonthDate.getDay()
+    const day = new Date(this.currentDate)
+    day.setDate(1 - firstWeekDayOfMonth)
 
-    for (let i = firstWeekDayOfMonth; i > 0; i--) {
-      const firstCurrentMonthDay = new Date(this.year, this.month, 1);
-      const dayBefore = new Date(firstCurrentMonthDay);
-      dayBefore.setDate(firstCurrentMonthDay.getDate() - i);
-      const parsableDate = `${this.month}-${dayBefore.getDate()}-${this.year}`;
-      const dayNumber = dayBefore.getDate();
-      const day = new Date(this.year, this.month - 1, dayNumber);
-
-
-      this.daysContainerTarget.innerHTML += this.makeDayElement(day, parsableDate, dayNumber);
+    for (let i = 1; i <= (7 * 6); i++) {
+      this.daysContainerTarget.innerHTML += this.makeDayElement(day);
+      day.setDate(day.getDate() + 1)
     }
   }
 
-  renderCurrentMonthDays() {
-    const daysInMonth = new Date(this.year, this.month + 1, 0).getDate();
-
-    for (let i = 1; i <= daysInMonth; i++) {
-      const day = new Date(this.year, this.month, i);
-      const parsableDate = `${this.month + 1}-${i}-${this.year}`
-
-      this.daysContainerTarget.innerHTML += this.makeDayElement(day, parsableDate, i);
-    }
-  }
-
-  makeDayElement(day, parsableDate, dayNumber) {
-    const classes = this.cssClassesFor(day)
-    return `<div class="${classes}" data-action="click->datefilter#selectDate" data-date="${parsableDate}">${dayNumber}</div>`
+  makeDayElement(day) {
+    const classes = this.cssClassesFor(day);
+    const month = day.getMonth() + 1;
+    const dayNumber = day.getDate();
+    const year = day.getFullYear();
+    const parsableDate = `${month}-${dayNumber}-${year}`;
+    return `<div class="${classes}" data-action="click->datefilter#selectDate" data-date="${parsableDate}">${dayNumber}</div>`;
   }
 
   cssClassesFor(day) {
-    if (this.isStartDate(day)) { return this.cssClasses.selectedStartDay; }
-    else if (this.isEndDate(day)) { return this.cssClasses.selectedEndDay; }
-    else if (this.isBetweenStartAndEnd(day)) { return this.cssClasses.selectedDay; }
-    else if (this.isPreviousMonth(day)) { return this.cssClasses.prevMonthDay; }
-    else { return this.cssClasses.currentMonthDay; }
+    if (this.isStartAndEnd(day)) return this.cssClasses.onlySelectedDay;
+    else if (this.isStartDate(day)) return this.cssClasses.selectedStartDay; 
+    else if (this.isEndDate(day)) return this.cssClasses.selectedEndDay; 
+    else if (this.isBetween(day)) return this.cssClasses.selectedDay; 
+    else if (this.isCurrentMonth(day)) return this.cssClasses.currentMonthDay; 
+    else return this.cssClasses.otherMonthDay; 
   }
 
+  isStartAndEnd(day) { return this.startDateEqualsEndDate() && day.getTime() === this.startDate.getTime(); }
   isStartDate(day) { return this.startDate && day.getTime() === this.startDate.getTime(); }
   isEndDate(day) { return this.endDate && day.getTime() === this.endDate.getTime(); }
-  isBetweenStartAndEnd(day) { return this.startDate && this.endDate && day > this.startDate && day < this.endDate; }
-  isPreviousMonth(day) { return day.getMonth() < this.month; }
+  isBetween(day) { return this.startDate && this.endDate && day > this.startDate && day < this.endDate; }
+  isCurrentMonth(day) { return day.getMonth() === this.currentDate.getMonth(); }
 
   selectDate(event) {
     event.stopPropagation();
@@ -106,7 +104,7 @@ export default class extends Controller {
       this.startDate = selectedDate;
       this.endDate = null;
     } else {
-      if (new Date(selectedDate) < new Date(this.startDate)) {
+      if (selectedDate < this.startDate) {
         this.endDate = this.startDate;
         this.startDate = selectedDate;
       } else {
@@ -118,26 +116,26 @@ export default class extends Controller {
   }
 
   updateInput() {
-    if (this.startDateEqualsEndDate()) { this.inputTarget.value = this.readableDateFrom(this.startDate); }
-    else if (this.startDate && this.endDate) { this.inputTarget.value = `${this.readableDateFrom(this.startDate)} -> ${this.readableDateFrom(this.endDate)}`; }
-    else if (this.startDate) { this.inputTarget.value = `${this.readableDateFrom(this.startDate)} ->`; }
-    else { this.inputTarget.value = ''; }
+    if (this.startDateEqualsEndDate()) this.inputTarget.value = this.readableDateFrom(this.startDate); 
+    else if (this.startDate && this.endDate) this.inputTarget.value = `${this.readableDateFrom(this.startDate)} -> ${this.readableDateFrom(this.endDate)}`; 
+    else if (this.startDate) this.inputTarget.value = `${this.readableDateFrom(this.startDate)} ->`; 
+    else this.inputTarget.value = ''; 
   }
 
   startDateEqualsEndDate() { return this.startDate && this.endDate && this.startDate.getTime() === this.endDate.getTime() }
 
   readableDateFrom(date) {
-    const opts = { weekday: "short", day: "numeric", month: "long" }
+    const opts = { weekday: "short", day: "numeric", month: "short" }
     return date.toLocaleDateString("fr-FR", opts)
   }
 
   setPrevMonth() {
-    this.today.setMonth(this.today.getMonth() - 1);
+    this.currentDate.setMonth(this.currentDate.getMonth() - 1);
     this.renderCalendar();
   }
 
   setNextMonth() {
-    this.today.setMonth(this.today.getMonth() + 1);
+    this.currentDate.setMonth(this.currentDate.getMonth() + 1);
     this.renderCalendar();
   }
 
