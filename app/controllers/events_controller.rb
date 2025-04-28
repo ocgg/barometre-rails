bff374a6eae723ed30c76d6d87ca9d119a0646f0
ocgg <olivier.g@lavache.com>
2025-04-28 13:04:33 +0200

    setup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -35,9 +35,26 @@ class EventsController < ApplicationController
 
   private
 
+  # REQUIRED: return the start and end limits of the collection as a 2 items array
+  def pagy_calendar_period(collection)
+    starting = collection.minimum(:date)
+    ending = collection.maximum(:date)
+    [ starting.in_time_zone, ending.in_time_zone ]
+  end
+
+  # REQUIRED: return the collection filtered by a time period
+  def pagy_calendar_filter(collection, from, to)
+    collection.where(date: from...to)
+  end
+
+  # OPTIONAL: return the array counts per time
+  def pagy_calendar_counts(collection, unit, from, to)
+    collection.group_by_period(unit, :date, range: from...to).count.values
+  end
+
   def set_events
-    @pagy, @events = pagy(policy_scope(Event), limit: 5)
-    authorize @events
+    events = authorize policy_scope(Event)
+    @calendar, @pagy, @events = pagy_calendar(events, day: {}, pagy: {})
     set_events_days
   end
 
