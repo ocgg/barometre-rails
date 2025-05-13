29bd14141dcb108ddb16df2bb713da1bfc46074c
ocgg <olivier.g@lavache.com>
2025-05-13 11:08:07 +0200

    basic venue form

--- app/javascript/controllers/new_event_form_controller.js
+++ app/javascript/controllers/new_event_form_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-event-form"
 export default class extends Controller {
-  static targets = ["eventFields", "eventsList", "addEventBtn"]
+  static targets = [
+    "eventFields",
+    "eventsList",
+    "addEventBtn",
+    "venueInput",
+    "venueResults",
+    "resultsContainer",
+    "noResult",
+    "venueTemplate"
+  ]
 
   connect() {
     this.eventsNumber = this.eventFieldsTargets.length;
@@ -19,5 +28,30 @@ export default class extends Controller {
     clone.querySelector(".errors")?.remove();
     clone.querySelector(".trash-btn").classList.remove("hidden")
     return clone
+  }
+
+  onVenueInput(event) {
+    const query = this.venueInputTarget.value;
+    const url = `/venues?q=${query}`;
+    const opts = {headers: { "Accept": "application/json" }};
+
+    fetch(url, opts)
+      .then(response => response.json())
+      .then((venues) => {
+        //this.venueResultsTarget.classList.toggle("hidden", false);
+        this.noResultTarget.classList.toggle("hidden", !(venues.length === 0));
+        this.resultsContainerTarget.innerHTML = '';
+        venues.forEach(venue => {
+          const clone = this.venueTemplateTarget.content.cloneNode(true);
+          clone.querySelector(".venue-name").innerText = venue.name;
+          clone.querySelector(".venue-address").innerText = `${venue.address} (${venue.city})`;
+          clone.querySelector("div").dataset.venue_id = venue.id;
+          this.resultsContainerTarget.appendChild(clone);
+        });
+      });
+  }
+
+  onVenueSelect(event) {
+    console.log(event.currentTarget.dataset.venue_id)
   }
 }
