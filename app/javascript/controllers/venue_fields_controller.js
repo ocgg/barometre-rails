0a3337079800d3a52960eb4fc8a3f25278538df9
Olivier G <zotri@hotmail.com>
2025-05-14 12:59:01 +0200

    lil refacto + hidden venue_id input

--- app/javascript/controllers/venue_fields_controller.js
+++ app/javascript/controllers/venue_fields_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="venue-fields"
 export default class extends Controller {
   static targets = [
+    "venueIdInput",
     "searchInput",
     "resultsContainer",
     "resultsList",
@@ -10,28 +11,34 @@ export default class extends Controller {
     "template"
   ]
 
-  onVenueInput(event) {
-    const query = this.searchInputTarget.value;
-    const url = `/venues?q=${query}`;
-    const opts = {headers: { "Accept": "application/json" }};
+  onVenueInput(_) {
+    const url = `/venues?q=${this.searchInputTarget.value}`;
+    this.fetchVenues(url);
+  }
 
+  fetchVenues(url) {
+    const opts = {headers: { "Accept": "application/json" }};
     fetch(url, opts)
       .then(response => response.json())
-      .then((venues) => {
-        //this.resultsContainerTarget.classList.toggle("hidden", false);
-        this.noResultTarget.classList.toggle("hidden", !(venues.length === 0));
-        this.resultsListTarget.innerHTML = '';
-        venues.forEach(venue => {
-          const clone = this.templateTarget.content.cloneNode(true);
-          clone.querySelector("li").dataset.venue_id = venue.id;
-          clone.querySelector(".venue-name").innerText = venue.name;
-          clone.querySelector(".venue-address").innerText = `${venue.address} (${venue.city})`;
-          this.resultsListTarget.appendChild(clone);
-        });
-      });
+      .then(venues => this.renderVenues(venues));
+  }
+
+  renderVenues(venues) {
+    this.noResultTarget.classList.toggle("hidden", !(venues.length === 0));
+    this.resultsListTarget.innerHTML = '';
+    venues.forEach(venue => this.renderVenue(venue));
+  }
+
+  renderVenue(venue) {
+    const clone = this.templateTarget.content.cloneNode(true);
+    clone.querySelector("li").dataset.venue_id = venue.id;
+    clone.querySelector(".venue-name").innerText = venue.name;
+    clone.querySelector(".venue-address").innerText = `${venue.address} (${venue.city})`;
+    this.resultsListTarget.appendChild(clone);
   }
 
   onVenueSelect(event) {
-    console.log(event.currentTarget.dataset.venue_id)
+    const venueId = event.currentTarget.dataset.venue_id;
+    this.venueIdInputTarget.value = venueId;
   }
 }
