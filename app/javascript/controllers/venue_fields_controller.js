94bab92d2a340830286a260cd0961aa4b3a412ee
Olivier G <zotri@hotmail.com>
2025-05-14 16:41:52 +0200

    lil refacto + venue selection

--- app/javascript/controllers/venue_fields_controller.js
+++ app/javascript/controllers/venue_fields_controller.js
@@ -5,40 +5,87 @@ export default class extends Controller {
   static targets = [
     "venueIdInput",
     "searchInput",
-    "resultsContainer",
     "resultsList",
     "noResult",
-    "template"
+    "template",
+    "dropdown",
+    "address",
+    "city"
   ]
 
   onVenueInput(_) {
-    const url = `/venues?q=${this.searchInputTarget.value}`;
-    this.fetchVenues(url);
+    this.showDropdown();
+    this.fetchVenues();
   }
 
-  fetchVenues(url) {
-    const opts = {headers: { "Accept": "application/json" }};
+  fetchVenues() {
+    const url = `/venues?q=${this.searchInputTarget.value}`;
+    const opts = { headers: { "Accept": "application/json" } };
     fetch(url, opts)
       .then(response => response.json())
-      .then(venues => this.renderVenues(venues));
+      .then(venues => {
+        this.venues = venues;
+        this.noResultTarget.classList.toggle("hidden", !(this.venues.length === 0));
+        this.renderVenues();
+      });
   }
 
-  renderVenues(venues) {
-    this.noResultTarget.classList.toggle("hidden", !(venues.length === 0));
+  renderVenues() {
     this.resultsListTarget.innerHTML = '';
-    venues.forEach(venue => this.renderVenue(venue));
+    this.venues.forEach(venue => this.renderVenue(venue));
   }
 
   renderVenue(venue) {
     const clone = this.templateTarget.content.cloneNode(true);
-    clone.querySelector("li").dataset.venue_id = venue.id;
+    const li = clone.querySelector("li");
+    li.dataset.id = venue.id;
+    li.dataset.name = venue.name;
     clone.querySelector(".venue-name").innerText = venue.name;
     clone.querySelector(".venue-address").innerText = `${venue.address} (${venue.city})`;
     this.resultsListTarget.appendChild(clone);
   }
 
   onVenueSelect(event) {
-    const venueId = event.currentTarget.dataset.venue_id;
-    this.venueIdInputTarget.value = venueId;
+    const venue_data = event.currentTarget.dataset;
+    this.setSelectedVenue(venue_data.id);
+    this.dropdownTarget.classList.add("hidden");
+  }
+
+  setSelectedVenue(venueId) {
+    const venue = this.venues.find(venue => venue.id == venueId)
+    this.setIdInput(venue);
+    this.setSearchInput(venue);
+    this.displaySelectedVenueInfos(venue);
+    this.hideDropdown();
+  }
+
+  setIdInput(venue) {
+    this.venueIdInputTarget.disabled = false;
+    this.venueIdInputTarget.value = venue.id;
+  }
+
+  setSearchInput(venue) {
+    this.searchInputTarget.value = venue.name;
+    this.searchInputTarget.disabled = true;
+  }
+
+  displaySelectedVenueInfos(venue) {
+    this.addressTarget.innerText = venue.address;
+    this.cityTarget.innerText = `${venue.city}`;
+  }
+
+  showDropdown() {
+    this.dropdownTarget.style.display = "block"
+  }
+
+  hideDropdown() {
+    this.dropdownTarget.style.display = "none"
+  }
+
+  onFocusOut(event) {
+    console.log(event)
+    if (this.dropdownTarget.contains(event.target)) return;
+
+    this.hideDropdown();
   }
 }
