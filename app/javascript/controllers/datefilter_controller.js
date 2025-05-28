f2af7722935b50be3d996a4d0b712a19d189f113
ocgg <olivier.g@lavache.com>
2025-05-28 13:21:29 +0200

    refacto: use datepicker lib

--- app/javascript/controllers/datefilter_controller.js
+++ app/javascript/controllers/datefilter_controller.js
@@ -1,66 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 import DatesManager from "datepicker/dates_manager"
 import Renderer from "datepicker/renderer"
+import Datepicker from "datepicker/datepicker"
 
 // Connects to data-controller="datefilter"
 export default class extends Controller {
   static targets = [
     "mainContainer",
-    "startDateInput",
-    "endDateInput",
     "button",
-    "calendarContainer",
-    "daysContainer",
-    "monthName", "prevMonth", "nextMonth",
+    "datepickerContainer",
     "clearInputBtn",
-    "dayTemplate",
-    "submit"
   ]
 
   connect() {
-    this.dates = new DatesManager(
-      this.startDateInputTarget.value,
-      this.endDateInputTarget.value
-    );
-    this.render = new Renderer(this.dates);
+    const opts = { autosubmit: true, range: true }
+    this.datepicker = new Datepicker(this.datepickerContainerTarget, opts);
 
-    this.mainContainerTarget.classList.toggle(this.render.css.calendarContainer, this.active);
-    this.updateValuesAndText()
-    this.renderCalendar();
+    this.datepicker.elts.startInput.dataset.action = "change->datefilter#onStartDateSelection"
+    this.datepicker.elts.endInput.dataset.action = "change->datefilter#onEndDateSelection"
+
+    this.containerClass = "bg-card-bg";
+    this.mainContainerTarget.classList.toggle(this.containerClass, this.datepicker.active);
     this.updateElementSize();
   }
 
-  get active() { return !this.startDateInputTarget.disabled }
-
-  get visible() { return !this.calendarContainerTarget.classList.contains("hidden") }
-
-  get startDate() { return this.startDateInputTarget.value }
-
-  get endDate() { return this.endDateInputTarget.value }
+  get visible() { return !this.datepickerContainerTarget.classList.contains("hidden") }
 
   setVisible(bool) {
-    this.calendarContainerTarget.classList.toggle("hidden", !bool);
+    this.datepickerContainerTarget.classList.toggle("hidden", !bool);
     this.mainContainerTarget.classList.toggle("max-md:translate-x-[-25%]", bool);
-    this.mainContainerTarget.classList.toggle(this.render.css.calendarContainer, bool || this.active);
+    this.mainContainerTarget.classList.toggle(this.containerClass, bool || this.datepicker.active);
     if (!bool) this.updateElementSize();
-  }
-
-  renderCalendar() {
-    this.daysContainerTarget.innerHTML = '';
-    this.monthNameTarget.textContent = this.render.monthName;
-    this.daysContainerTarget.innerHTML = this.render.dayElements();
-  }
-
-  activate() {
-    this.startDateInputTarget.disabled = false;
-    this.endDateInputTarget.disabled = false;
-    this.mainContainerTarget.classList.toggle(this.render.css.calendarContainer, true);
-  }
-
-  desactivate() {
-    this.startDateInputTarget.disabled = true;
-    this.endDateInputTarget.disabled = true;
-    this.mainContainerTarget.classList.toggle(this.render.css.calendarContainer, this.visible);
   }
 
   updateElementSize() {
@@ -69,96 +39,41 @@ export default class extends Controller {
     this.element.style.height = `${Math.floor(size.height)}px`;
   }
 
-  updateDates(start, end) {
-    this.dates.start = start;
-    this.dates.end = end;
-    this.updateValuesAndText();
-    this.renderCalendar();
-  }
-
-  updateValuesAndText() {
-    if (this.dates.startEqualsEnd) this.setStartEqualsEndValuesAndText();
-    else if (this.dates.startWithoutEnd) this.setStartWithoutEndValuesAndText();
-    else if (this.dates.startAndEnd) this.setStartAndEndValuesAndText();
-    else this.setNoStartNoEndValuesAndText();
-  }
-
-  setStartEqualsEndValuesAndText() {
-    this.startDateInputTarget.value = this.render.parsableStart;
-    this.endDateInputTarget.value = this.render.parsableEnd;
-    this.buttonTarget.textContent = this.render.readableStart;
-    this.clearInputBtnTarget.classList.toggle("hidden", false);
-  }
-
-  setStartWithoutEndValuesAndText() {
-    this.startDateInputTarget.value = this.render.parsableStart;
-    this.endDateInputTarget.value = this.render.parsableStart;
-    this.buttonTarget.textContent = `${this.render.readableStart} ➞ ...`;
-    this.clearInputBtnTarget.classList.toggle("hidden", false);
-  }
-
-  setStartAndEndValuesAndText() {
-    this.startDateInputTarget.value = this.render.parsableStart;
-    this.endDateInputTarget.value = this.render.parsableEnd;
-    this.buttonTarget.textContent = `${this.render.readableStart} ➞ ${this.render.readableEnd}`;
-    this.clearInputBtnTarget.classList.toggle("hidden", false);
-  }
-
-  setNoStartNoEndValuesAndText() {
-    this.startDateInputTarget.value = null;
-    this.endDateInputTarget.value = null;
-    this.buttonTarget.textContent = "Filtrer par date";
-    this.clearInputBtnTarget.classList.toggle("hidden", true);
-  }
-
   onButtonClick(_) { this.setVisible(!this.visible) }
 
-  selectDate(event) {
-    event.stopPropagation();
+  onStartDateSelection(event) {
+    if (!event.target.value) return;
 
-    const selectedDate = new Date(event.currentTarget.dataset.date);
-    if (!this.dates.start || this.dates.startAndEnd) {
-      this.updateDates(selectedDate, null);
-      if (this.active) this.desactivate();
-    }
-    else if (selectedDate < this.dates.start) {
-      this.updateDates(selectedDate, this.dates.start);
-      if (!this.active) this.activate();
-      this.submit();
-    }
-    else {
-      this.updateDates(this.dates.start, selectedDate);
-      if (!this.active) this.activate();
-      this.submit();
-    }
+    const date = new Date(event.target.value);
+    this.startDateString = this.readableStringFrom(date);
+    this.buttonTarget.textContent = `${this.startDateString} ➞ ...`;
+    this.clearInputBtnTarget.classList.toggle("hidden", false)
   }
 
-  submit() { this.submitTarget.click() }
+  onEndDateSelection(event) {
+    if (!event.target.value) return;
 
-  setPrevMonth(_) {
-    this.dates.toPrevMonth();
-    this.renderCalendar();
+    const date = new Date(event.target.value);
+    this.endDateString = this.readableStringFrom(date);
+    this.buttonTarget.textContent = `${this.startDateString} ➞ ${this.endDateString}`;
   }
 
-  setNextMonth(_) {
-    this.dates.toNextMonth();
-    this.renderCalendar();
-  }
-
-  selectMonth(_) {
-    if (!this.active) this.activate();
-    const start = this.dates.firstDateOfMonth;
-    const end = this.dates.lastDateOfMonth;
-    this.updateDates(start, end);
-    this.submit();
+  readableStringFrom(date) {
+    const opts = { day: "numeric", month: "long" };
+    return date.toLocaleDateString("fr-FR", opts);
   }
 
   clearInput(event) {
     event.stopPropagation();
-    this.updateDates(null, null);
-    this.desactivate();
+    this.datepicker.reset();
+    this.resetButton()
     this.setVisible(false);
-    this.submit();
+    this.datepicker.submit();
+  }
+
+  resetButton() {
+    this.buttonTarget.textContent = "Filtrer par dates";
+    this.clearInputBtnTarget.classList.toggle("hidden", true)
   }
 
   handleFocusOut(event) {
