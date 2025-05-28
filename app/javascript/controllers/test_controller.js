74bc03a7739bed0dd3028599669bda1786850429
ocgg <olivier.g@lavache.com>
2025-05-28 13:28:13 +0200

    remove test code

--- app/javascript/controllers/test_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-import Datepicker from "datepicker/datepicker"
-
-// Connects to data-controller="test"
-export default class extends Controller {
-  connect() {
-    new Datepicker(this.element);
-  }
-}
