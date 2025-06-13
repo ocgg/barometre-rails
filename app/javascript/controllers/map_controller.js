e8bbf09f91aa1832a27dbae13e043595788501bf
Pascal Billerach <p.billerach@gmail.com>
2025-06-13 16:50:07 +0200

    config leaflet

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    var map = L.map('map').setView([47.216671, -1.55], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    const url = `/venues/`;
+    const opts = { headers: { "Accept": "application/json" } };
+    fetch(url, opts)
+      .then(response => response.json())
+      .then((data => {
+        data.forEach(venue => {
+          L.marker([venue.latitude, venue.longitude])
+            .addTo(map)
+            .bindPopup(`<b>${venue.name}</b><br>${venue.address}`);
+        });
+      }));
+  }
+}
