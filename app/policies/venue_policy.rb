740e1e24819c2d086eef84fc76a9f576fae718bc
ocgg <olivier.g@lavache.com>
2025-06-13 11:53:04 +0200

    fix: dont show events with unverified venues

--- app/policies/venue_policy.rb
+++ app/policies/venue_policy.rb
@@ -25,7 +25,7 @@ class VenuePolicy < ApplicationPolicy
 
   class Scope < ApplicationPolicy::Scope
     def resolve
-      @user.admin? ? scope.includes(:events).all : scope.where(verified: true)
+      @user.admin? ? scope.includes(:events).all : scope.where(verified: true, venue: {verified: true})
     end
   end
 end
