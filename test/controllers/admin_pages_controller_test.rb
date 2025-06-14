7ea81b43ad84e87908710d13520922b143f5a24c
ocgg <olivier.g@lavache.com>
2025-06-14 12:39:20 +0200

    vibecoded tests for AdminPagesController

--- test/controllers/admin_pages_controller_test.rb
+++ test/controllers/admin_pages_controller_test.rb
@@ -1,8 +1,50 @@
 require "test_helper"
 
 class AdminPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get unverified" do
-    get admin_pages_unverified_url
+  setup do
+    @admin = users(:admin)
+    @visitor = User.new(role: "visitor")
+  end
+
+  test "visitor should not access unverified page" do
+    get unverified_url
+    assert_redirected_to new_session_url
+  end
+
+  test "visitor should not access unverified page with events section" do
+    get unverified_url(section: "events")
+    assert_redirected_to new_session_url
+  end
+
+  test "visitor should not access unverified page with venues section" do
+    get unverified_url(section: "venues")
+    assert_redirected_to new_session_url
+  end
+
+  test "admin should access unverified page" do
+    sign_in_as_admin
+    get unverified_url
     assert_response :success
+    assert_select "a[href=?]", unverified_events_path
+  end
+
+  test "admin should access unverified page with events section" do
+    sign_in_as_admin
+    get unverified_url(section: "events")
+    assert_response :success
+    assert_select "a[href=?]", unverified_events_path
+  end
+
+  test "admin should access unverified page with venues section" do
+    sign_in_as_admin
+    get unverified_url(section: "venues")
+    assert_response :success
+    assert_select "a[href=?]", unverified_venues_path
+  end
+
+  private
+
+  def sign_in_as_admin
+    post session_url, params: { email_address: @admin.email_address, password: "password" }
   end
 end
