232844fe1f93e6848680616c0e08d6e29f0de9c0
ocgg <olivier.g@lavache.com>
2025-08-12 17:40:01 +0200

    contact form submission base

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,27 @@
 class PagesController < ApplicationController
   allow_unauthenticated_access
 
+  def about
+  end
+
   def contact
   end
 
-  def about
+  def contact_form_submit
+    contact_params = contact_form_params
+
+    email = contact_params[:email]
+    message = contact_params[:message]
+    attachment = contact_params[:attachment]
+
+    # TODO: manage attachment
+    # TODO: Send email
+    # TODO: notify user
+  end
+
+  private
+
+  def contact_form_params
+    params.require(:contact_form).permit(:email, :message, :attachment)
   end
 end
