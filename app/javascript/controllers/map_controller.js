4113a12cacd18401108a67a7c19ba1c2c0a60c16
ocgg <olivier.g@lavache.com>
2025-09-08 13:18:52 +0200

    map marker icon

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,23 +1,47 @@
 import { Controller } from "@hotwired/stimulus"
+import "leaflet"
+import "leaflet-css"
 
 export default class extends Controller {
+  static values = { markerIcon: String }
   connect() {
-    var map = L.map('map').setView([47.216671, -1.55], 13);
+    this.initMap();
+    this.fetchVenues();
+  }
+
+  initMap() {
+    this.map = L.map('map', {
+      zoomControl: false,
+    }).setView([47.216671, -1.55], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    }).addTo(this.map);
 
+    this.markerIcon = L.icon({
+      iconUrl: this.markerIconValue,
+      iconSize: [38, 38],
+      iconAnchor: [19, 38],
+      popupAnchor: [0, -19] // relative to iconAnchor
+      // shadowUrl: this.markerIconValue,
+      // shadowSize: [50, 64],
+      // shadowAnchor: [4, 62],
+    });
+
+    L.control.zoom({ position: "bottomright" }).addTo(this.map);
+  }
+
+  fetchVenues() {
     const url = "/api/venues";
     const opts = { headers: { "Accept": "application/json" } };
     fetch(url, opts)
       .then(response => response.json())
       .then((data => {
         data.forEach(venue => {
-          L.marker([venue.latitude, venue.longitude])
-            .addTo(map)
-            .bindPopup(`<b>${venue.name}</b><br>${venue.address}`);
+          L.marker([venue.latitude, venue.longitude], { icon: this.markerIcon })
+            .bindPopup(`<b>${venue.name}</b><br>${venue.address}`)
+            .addTo(this.map)
         });
       }));
   }
