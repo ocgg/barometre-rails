dabe6cd442dc9b724a681eccebeb08b064035cff
ocgg <olivier.g@lavache.com>
2025-09-10 13:14:59 +0200

    style on evt-card:hover & marker:hover

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,14 +3,16 @@ import L from "leaflet"
 import "leaflet-css"
 
 export default class extends Controller {
-  static targets = ["venuesData"]
+  static targets = ["venuesData", "eventElt"]
 
   static values = {
     markerIcon: String,
+    markerIconHover: String,
   }
 
   initialize() {
     this.venuesMarkers = {};
+
     this.map = L.map('map', { attributionControl: false }).setView([47.216671, -1.55], 13);
 
     this.map.zoomControl.setPosition("bottomright");
@@ -26,7 +28,7 @@ export default class extends Controller {
 
     this.markerIcon = L.icon({
       iconUrl: this.markerIconValue,
-      className: "hover:cursor-pointer",
+      className: "hover:cursor-pointer rounded-full",
       iconSize: [38, 38],
       iconAnchor: [19, 38],
       popupAnchor: [0, -19] // relative to iconAnchor
@@ -41,7 +43,6 @@ export default class extends Controller {
   venuesDataTargetConnected() {
     this.removeMarkers();
     this.createMarkers();
-    this.addMarkersToMap();
     this.setBoundaries(this.markers);
   }
 
@@ -58,24 +59,53 @@ export default class extends Controller {
   createMarker(venue) {
     const marker = L.marker([venue.latitude, venue.longitude], { icon: this.markerIcon })
       .bindPopup(`<b>${venue.name}</b><br>${venue.address}`)
-    this.venuesMarkers[venue.id] = marker
-  }
-
-  addMarkersToMap() {
-    this.markers.forEach(marker => marker.addTo(this.map))
+      .addTo(this.map)
+    marker._icon.addEventListener("mouseover", this.onEventMouseOver.bind(this))
+    marker._icon.addEventListener("mouseleave", this.onEventMouseLeave.bind(this))
+    marker._icon.dataset.venueId = venue.id;
+    this.venuesMarkers[venue.id] = marker;
   }
 
   setBoundaries(points) {
-    if (this.venuesMarkers.length === 0) return;
+    if (points.length === 0) return;
 
     const group = new L.featureGroup(points);
-    const opts = { animate: true, maxZoom: 13, paddingTopLeft: [384, 0] }
-    this.map.fitBounds(group.getBounds().pad(0.5), opts)
+    const opts = {
+      maxZoom: 13,
+      paddingTopLeft: [384, 0]
+    }
+    this.map.flyToBounds(group.getBounds().pad(0.5), opts)
   }
 
   onEventClick(event) {
     const venueId = event.currentTarget.dataset.venueId;
     const venueMarker = this.venuesMarkers[venueId]
     this.setBoundaries([venueMarker])
+  }
+
+  onEventMouseOver(event) {
+    const venueId = event.target.dataset.venueId
+    const marker = this.venuesMarkers[venueId]
+    const events = this.eventEltTargets.filter(elt => elt.dataset.venueId == venueId)
+    this.addHoverClass(events, marker._icon)
+  }
+
+  onEventMouseLeave(event) {
+    const venueId = event.target.dataset.venueId
+    const marker = this.venuesMarkers[venueId]
+    const events = this.eventEltTargets.filter(elt => elt.dataset.venueId == venueId)
+    this.removeHoverClass(events, marker._icon)
+  }
+
+  addHoverClass(eventElts, markerElt) {
+    eventElts.forEach(elt => elt.classList.add("border-yellow"))
+    markerElt.classList.add("bg-yellow");
+    markerElt.style.zIndex += 250;
+  }
+
+  removeHoverClass(eventElts, markerElt) {
+    eventElts.forEach(elt => elt.classList.remove("border-yellow"))
+    markerElt.classList.remove("bg-yellow");
+    markerElt.style.zIndex -= 250;
   }
 }
