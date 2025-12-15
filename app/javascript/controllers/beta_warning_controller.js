2812fd57d8ddaeafe22ee631a6f9ee12830e6c11
ocgg <olivier.g@lavache.com>
2025-12-15 18:46:45 +0100

    betatest popup

--- app/javascript/controllers/beta_warning_controller.js
+++ app/javascript/controllers/beta_warning_controller.js
@@ -4,7 +4,7 @@ import Modal from "modal/main"
 // Connects to data-controller="beta-warning"
 export default class extends Controller {
   connect() {
-    const modal = new Modal(this.element, this.configValue);
+    const modal = new Modal(this.element);
     modal.afterClose = () => sessionStorage.setItem("betaWarned", true);
 
     const hasBeenWarned = sessionStorage.getItem("betaWarned") === "true"
